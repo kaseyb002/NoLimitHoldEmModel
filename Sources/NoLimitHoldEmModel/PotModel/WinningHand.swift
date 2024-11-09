5f80e5b4647553cb8d3154ce13ec67b5f905c2b4
Kasey Baughan <kaseyb002@yahoo.com>
2024-11-09 11:39:36 -0500

    fix decoding

--- Sources/NoLimitHoldEmModel/PotModel/WinningHand.swift
+++ Sources/NoLimitHoldEmModel/PotModel/WinningHand.swift
@@ -4,6 +4,11 @@ public struct WinningHand: Hashable, Codable {
     public let playerID: String
     public let pokerHand: PokerHand
     
+    public enum CodingKeys: String, CodingKey {
+        case playerID = "playerId"
+        case pokerHand
+    }
+    
     public init(
         playerID: String,
         pokerHand: PokerHand
